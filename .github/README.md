d8a5d2ad6b2391142bdc090d52bb943764013373
Ben Ashby <bashby@ximasoftware.com>
2023-05-21 13:40:00 -0600

    Initial Commit

---
+++ .github/README.md
@@ -0,0 +1,5 @@
+# SharedMedia_Juksie
+* Adds some sound files
+
+## Download
+<https://www.curseforge.com/wow/addons/sharedmedia_juksie>
---
+++ MyMedia.lua
@@ -0,0 +1,36 @@
+local LSM = LibStub("LibSharedMedia-3.0") 
+
+-- ----- 
+-- BACKGROUND 
+-- ----- 
+
+-- ----- 
+--  BORDER 
+-- ---- 
+
+-- -----
+--   FONT
+-- -----
+
+-- -----
+--   SOUND
+-- -----
+
+LSM:Register("sound", "|cFFFF0000Starfall|r", [[Interface\Addons\SharedMedia_Juksie\sound\starfall.ogg]])
+LSM:Register("sound", "|cFFFF0000RezTheHealer|r", [[Interface\Addons\SharedMedia_Juksie\sound\rez-healer.ogg]])
+LSM:Register("sound", "|cFFFF0000RezTheTank|r", [[Interface\Addons\SharedMedia_Juksie\sound\rez-tank.ogg]])
+LSM:Register("sound", "|cFFFF0000RezDps|r", [[Interface\Addons\SharedMedia_Juksie\sound\rez-dps.ogg]])
+LSM:Register("sound", "|cFFFF0000Ctrl4|r", [[Interface\Addons\SharedMedia_Juksie\sound\Ctrl4.ogg]])
+LSM:Register("sound", "|cFFFF0000DodgeLeap|r", [[Interface\Addons\SharedMedia_Juksie\sound\HoI_DodgeLeap.ogg]])
+LSM:Register("sound", "|cFFFF0000SomeoneDied|r", [[Interface\Addons\SharedMedia_Juksie\sound\someone-died.ogg]])
+LSM:Register("sound", "|cFFFF0000InnervateHealer|r", [[Interface\Addons\SharedMedia_Juksie\sound\innervate-healer.ogg]])
+LSM:Register("sound", "|cFFFF0000TankDied|r", [[Interface\Addons\SharedMedia_Juksie\sound\tank-died.ogg]])
+LSM:Register("sound", "|cFFFF0000HealerDied|r", [[Interface\Addons\SharedMedia_Juksie\sound\healer-died.ogg]])
+LSM:Register("sound", "|cFFFF0000DamagerDied|r", [[Interface\Addons\SharedMedia_Juksie\sound\damager-died.ogg]])
+LSM:Register("sound", "|cFFFF0000SootheRagestorm|r", [[Interface\Addons\SharedMedia_Juksie\sound\soothe-ragestorm.ogg]])
+LSM:Register("sound", "|cFFFF0000BeBehindTank|r", [[Interface\Addons\SharedMedia_Juksie\sound\be-behind-tank.ogg]])
+
+LSM:Register("sound", "|cFF0000FFDropPuddleOnLeft|r", [[Interface\Addons\SharedMedia_Juksie\sound\drop-puddle-on-left.ogg]])
+LSM:Register("sound", "|cFF0000FFDropPuddleOnRight|r", [[Interface\Addons\SharedMedia_Juksie\sound\drop-puddle-on-right.ogg]])
+LSM:Register("sound", "|cFF0000FFDragBeamToLeft|r", [[Interface\Addons\SharedMedia_Juksie\sound\drag-beam-to-left.ogg]])
+LSM:Register("sound", "|cFF0000FFDragBeamToRight|r", [[Interface\Addons\SharedMedia_Juksie\sound\drag-beam-to-right.ogg]])
