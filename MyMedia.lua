fa06dbefe8725820d2c257048bda52436b8ddf7c
Ben Ashby <bashby@ximasoftware.com>
2023-07-12 17:35:47 -0600

    Added several sounds

--- MyMedia.lua
+++ MyMedia.lua
@@ -1,8 +1,35 @@
 local LSM = LibStub("LibSharedMedia-3.0")
 
 -- -----
+-- BACKGROUND
+-- -----
+
+-- -----
+--  BORDER
+-- ----
+
+-- -----
+--   FONT
+-- -----
+
+-- -----
 --   SOUND
 -- -----
+
+
+
+---
+--- Monk Sounds
+---
+
+LSM:Register("sound", "|cFF00FF98DiffuseMagic|r", [[Interface\Addons\SharedMedia_Juksie\sound\diffuse-magic.ogg]])
+LSM:Register("sound", "|cFF00FF98UseBlackoutKick|r", [[Interface\Addons\SharedMedia_Juksie\sound\use-blackout-kick.ogg]])
+LSM:Register("sound", "|cFF00FF98Blackout|r", [[Interface\Addons\SharedMedia_Juksie\sound\blackout.ogg]])
+
+
+
+
+
 
 LSM:Register("sound", "|cFFFF0000Starfall|r", [[Interface\Addons\SharedMedia_Juksie\sound\starfall.ogg]])
 LSM:Register("sound", "|cFFFF0000RezTheHealer|r", [[Interface\Addons\SharedMedia_Juksie\sound\rez-healer.ogg]])
@@ -22,3 +49,48 @@ LSM:Register("sound", "|cFF0000FFDropPuddleOnLeft|r", [[Interface\Addons\SharedM
 LSM:Register("sound", "|cFF0000FFDropPuddleOnRight|r", [[Interface\Addons\SharedMedia_Juksie\sound\drop-puddle-on-right.ogg]])
 LSM:Register("sound", "|cFF0000FFDragBeamToLeft|r", [[Interface\Addons\SharedMedia_Juksie\sound\drag-beam-to-left.ogg]])
 LSM:Register("sound", "|cFF0000FFDragBeamToRight|r", [[Interface\Addons\SharedMedia_Juksie\sound\drag-beam-to-right.ogg]])
+
+
+LSM:Register("sound", "|cFF0000FFEmbraceOfNothingnessOnMe|r", [[Interface\Addons\SharedMedia_Juksie\sound\embrace-of-nothingness-me.ogg]])
+LSM:Register("sound", "|cFF0000FFMassDisintegrateOnOthers|r", [[Interface\Addons\SharedMedia_Juksie\sound\mass-disintegrate-on-others.ogg]])
+LSM:Register("sound", "|cFF0000FFMassDisintegrateOnMe|r", [[Interface\Addons\SharedMedia_Juksie\sound\mass-disintegrate-on-me.ogg]])
+LSM:Register("sound", "|cFF0000FFSearingBreath|r", [[Interface\Addons\SharedMedia_Juksie\sound\searing-breath.ogg]])
+LSM:Register("sound", "|cFF0000FFGlitteringSurge|r", [[Interface\Addons\SharedMedia_Juksie\sound\glittering-surge.ogg]])
+LSM:Register("sound", "|cFF0000FFAbyssalBreath|r", [[Interface\Addons\SharedMedia_Juksie\sound\abyssal-breath.ogg]])
+LSM:Register("sound", "|cFF0000FFVoidBomb|r", [[Interface\Addons\SharedMedia_Juksie\sound\void-bomb.ogg]])
+LSM:Register("sound", "|cFF0000FFScouringEternity|r", [[Interface\Addons\SharedMedia_Juksie\sound\scouring-eternity.ogg]])
+LSM:Register("sound", "|cFF0000FFEmbraceOfNothingnessOnOthers|r", [[Interface\Addons\SharedMedia_Juksie\sound\embrace-of-nothingness-others.ogg]])
+LSM:Register("sound", "|cFF0000FFInfiniteDuress|r", [[Interface\Addons\SharedMedia_Juksie\sound\infinite-duress.ogg]])
+LSM:Register("sound", "|cFF0000FFHurtlingBArrage|r", [[Interface\Addons\SharedMedia_Juksie\sound\hurtling-barrage.ogg]])
+LSM:Register("sound", "|cFF0000FFVoidBombSpawning|r", [[Interface\Addons\SharedMedia_Juksie\sound\void-bomb-spawning.ogg]])
+LSM:Register("sound", "|cFF0000FFCCIncorporeal|r", [[Interface\Addons\SharedMedia_Juksie\sound\cc-incorporeal.ogg]])
+LSM:Register("sound", "|cFF0000FFInfusedGlobules|r", [[Interface\Addons\SharedMedia_Juksie\sound\infused-globules.ogg]])
+LSM:Register("sound", "|cFF0000FFPowerOverload|r", [[Interface\Addons\SharedMedia_Juksie\sound\power-overload.ogg]])
+LSM:Register("sound", "|cFF0000FFFrostCycloneTimer|r", [[Interface\Addons\SharedMedia_Juksie\sound\frost-cyclone-timer.ogg]])
+LSM:Register("sound", "|cFF0000FFHailstorm|r", [[Interface\Addons\SharedMedia_Juksie\sound\hide-behind-strong-ice.ogg]])
+LSM:Register("sound", "|cFF0000FFSavageCharge|r", [[Interface\Addons\SharedMedia_Juksie\sound\savage-charge.ogg]])
+LSM:Register("sound", "|cFF0000FFShatteringBellow|r", [[Interface\Addons\SharedMedia_Juksie\sound\shattering-bellow.ogg]])
+LSM:Register("sound", "|cFF0000FFSeaSpout|r", [[Interface\Addons\SharedMedia_Juksie\sound\sea-spout.ogg]])
+LSM:Register("sound", "|cFF0000FFRatTraps|r", [[Interface\Addons\SharedMedia_Juksie\sound\rat-traps.ogg]])
+LSM:Register("sound", "|cFF0000FFMarkedForButchery|r", [[Interface\Addons\SharedMedia_Juksie\sound\marked-for-butchery.ogg]])
+LSM:Register("sound", "|cFF0000FFGashFrenzy|r", [[Interface\Addons\SharedMedia_Juksie\sound\gash-frenzy.ogg]])
+LSM:Register("sound", "|cFF0000FFStinkBreath|r", [[Interface\Addons\SharedMedia_Juksie\sound\stink-breath.ogg]])
+LSM:Register("sound", "|cFF0000FFDodgeOrangeSwirl|r", [[Interface\Addons\SharedMedia_Juksie\sound\dodge-orange-swirl.ogg]])
+LSM:Register("sound", "|cFF0000FFDodgeBrownSwirl|r", [[Interface\Addons\SharedMedia_Juksie\sound\dodge-brown-swirl.ogg]])
+LSM:Register("sound", "|cFF0000FFBearFormDefensive|r", [[Interface\Addons\SharedMedia_Juksie\sound\bear-form-defensive.ogg]])
+LSM:Register("sound", "|cFF0000FFBigDamage|r", [[Interface\Addons\SharedMedia_Juksie\sound\big-damage.ogg]])
+LSM:Register("sound", "|cFF0000FFTentacleSpawning|r", [[Interface\Addons\SharedMedia_Juksie\sound\tentacles-spawning.ogg]])
+LSM:Register("sound", "|cFF0000FFPrepareForHands|r", [[Interface\Addons\SharedMedia_Juksie\sound\prepare-for-hands.ogg]])
+LSM:Register("sound", "|cFF0000FFHands|r", [[Interface\Addons\SharedMedia_Juksie\sound\hands.ogg]])
+LSM:Register("sound", "|cFF0000FFBileFrontal|r", [[Interface\Addons\SharedMedia_Juksie\sound\bile-frontal.ogg]])
+LSM:Register("sound", "|cFF0000FFBloodswarmerFixate|r", [[Interface\Addons\SharedMedia_Juksie\sound\bloodswarmer-fixate.ogg]])
+
+LSM:Register("sound", "|cFF00FF00FreeRegrowth|r", [[Interface\Addons\SharedMedia_Juksie\sound\free-regrowth.ogg]])
+LSM:Register("sound", "|cFF00FF00SwarmOnHighlightedFrame|r", [[Interface\Addons\SharedMedia_Juksie\sound\swarm-on-highlighted-frame.ogg]])
+LSM:Register("sound", "|cFF00FF00SwarmOnTank|r", [[Interface\Addons\SharedMedia_Juksie\sound\swarm-on-tank.ogg]])
+LSM:Register("sound", "|cFF00FF00SwarmOnFrame|r", [[Interface\Addons\SharedMedia_Juksie\sound\swarm-on-frame.ogg]])
+LSM:Register("sound", "|cFF00FF00Swarm|r", [[Interface\Addons\SharedMedia_Juksie\sound\swarm.ogg]])
+LSM:Register("sound", "|cFF00FF00HealToFull|r", [[Interface\Addons\SharedMedia_Juksie\sound\heal-to-full.ogg]])
+LSM:Register("sound", "|cFF00FF00DispelAfflicted|r", [[Interface\Addons\SharedMedia_Juksie\sound\dispel-afflicted.ogg]])
+LSM:Register("sound", "|cFF00FF00AfflictedSpawning|r", [[Interface\Addons\SharedMedia_Juksie\sound\afflicted-spawning.ogg]])
+LSM:Register("sound", "|cFF00FF00CureAfflicted|r", [[Interface\Addons\SharedMedia_Juksie\sound\cure-afflicted.ogg]])
